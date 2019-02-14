27d5fc67f5ef0f6524d9ab2874affe474f698296
hongs1234 <47427352+hongs1234@users.noreply.github.com>
2019-02-14 14:25:23 -0500

    Add files via upload

--- Drawing_from_Sketch.py
+++ Drawing_from_Sketch.py
@@ -11,15 +11,20 @@ arcade.start_render()
 
 #Sun
 arcade.draw_circle_filled(540, 380, 50, arcade.color.YELLOW)
+
 #Grass
 arcade.draw_xywh_rectangle_filled(0, 0, 640, 100, arcade.color.GREEN)
-#Tree Trunks
+
+#Left Tree
 arcade.draw_xywh_rectangle_filled(515, 75, 20, 75, arcade.color.BROWN)
-arcade.draw_xywh_rectangle_filled(440, 75, 20, 75, arcade.color.BROWN)
-arcade.draw_xywh_rectangle_filled(190, 75, 20, 75, arcade.color.BROWN)
-#Tree Leaves
 arcade.draw_triangle_filled(485, 150, 525, 225, 565, 150, arcade.color.DARK_GREEN)
+
+#Middle Tree
+arcade.draw_xywh_rectangle_filled(440, 75, 20, 75, arcade.color.BROWN)
 arcade.draw_triangle_filled(410, 150, 450, 225, 490, 150, arcade.color.DARK_GREEN)
+
+#Right Tree
+arcade.draw_xywh_rectangle_filled(190, 75, 20, 75, arcade.color.BROWN)
 arcade.draw_triangle_filled(160, 150, 200, 225, 240, 150, arcade.color.DARK_GREEN)
 
 # End drawing
